4b82779897a500c356f05b657aad044e10aa19e8
Jackles1234 <jack.welsh@drake.edu>
2024-09-03 10:32:41 -0500

    9/3/24: 1945

---
+++ 2024/September/sum_digits_w_transformations_1945.py
@@ -0,0 +1,13 @@
+from string import ascii_lowercase
+class Solution(object):
+    def transform(self, num_str):
+        split_nums = sum([int(char) for char in num_str])
+        return str(split_nums)
+    def getLucky(self, s, k):   
+        num_str = ""
+        for i in s:
+            num_str+= str(ord(i) - 96)
+
+        for i in range(k):
+            num_str = self.transform(num_str)
+        return int(num_str)
