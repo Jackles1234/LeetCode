fe6f90afbb2ea4cdc7507d4a9fd62586277e1ddb
Jackles1234 <jack.welsh@drake.edu>
2024-09-10 12:56:37 -0500

    9/10/24: Neetcode

--- 2024/September/find_missing_observations_2028.py
+++ 2024/September/find_missing_observations_2028.py
@@ -3,11 +3,12 @@ class Solution(object):
         return (sum(rolls + return_list))/(len(rolls)+len(return_list))
 
     def missingRolls(self, rolls, mean, n):
-        return_list = [5 for _ in range(n)]
-        print(self.get_comb_mean(rolls, return_list))
-        if mean < self.get_comb_mean(rolls, return_list):
-            return []
-
+        for i in range(1, 6, 1):
+            return_list = [i for _ in range(n)]
+            print(self.get_comb_mean(rolls, return_list))
+            if mean < self.get_comb_mean(rolls, return_list):
+                break
+        while 
 
         
 
