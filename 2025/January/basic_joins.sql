d309984e0fe5b89bbf9278993a720232371e4c11
Jackles1234 <jack.welsh.j@gmail.com>
2025-02-01 09:46:45 -0600

    2/1/25

--- 2025/January/basic_joins.sql
+++ 2025/January/basic_joins.sql
@@ -47,3 +47,12 @@ ON
     DATEDIFF(w1.recordDate, w2.recordDate) = 1
 WHERE 
     w1.temperature > w2.temperature;
+
+
+--Write your MySQL query statement below
+SELECT a1.machine_id, round(avg(a2.timestamp-a1.timestamp), 3) as processing_time 
+FROM Activity a1
+JOIN Activity a2 
+ON a1.machine_id=a2.machine_id and a1.process_id=a2.process_id
+AND a1.activity_type='start' and a2.activity_type='end'
+GROUP BY a1.machine_id
